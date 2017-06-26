70c992c037d1c725d070c7e6f957fadde2a9a6eb
benjamint@qualtrics.com <benjamint@qualtrics.com>
2017-06-25 23:50:14 -0700

    Initial commit for bookmark tutorial

---
+++ tut-bookmarks/src/main/java/com/example/tutbookmarks/TutBookmarksApplication.java
@@ -0,0 +1,12 @@
+package com.example.tutbookmarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutBookmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutBookmarksApplication.class, args);
+	}
+}
