c7a23f00c4e0977df8004ae65b776fb5f61ce74e
BJ Thompson <benjamint@qualtrics.com>
2017-12-06 08:24:30 -0800

    Using join to say hi to more than just 1 person

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,10 +1,11 @@
 (ns clojure-noob.core
   (:gen-class))
+(use '[clojure.string :only (join split)])
 
 (defn -main
   "I don't do a whole lot ... yet."
   ([& args]
-  (def userName (first args))
+  (def userName (join ", " args))
   (println (str "Hello " userName "!")))
   ([]
   (-main "World")))
